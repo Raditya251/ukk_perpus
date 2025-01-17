dadc7f43897d75a1abdff0afda0c6831bf106156
radit <raditya25@gmail.com>
2025-01-17 14:19:42 +0700

    erd db clear

--- app/Models/Pinjam.php
+++ app/Models/Pinjam.php
@@ -1,11 +1,22 @@
 <?php
 
 namespace App\Models;
-
+use App\Models\Buku;
+use App\Models\Users;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Pinjam extends Model
 {
     use HasFactory;
+
+    public function user()
+    {
+        return $this->belongTo(User::class);
+    }
+
+    public function buku()
+    {
+        return $this->belongTo(Buku::class);
+    }
 }
--- app/Models/Ulasan.php
+++ app/Models/Ulasan.php
@@ -2,10 +2,24 @@
 
 namespace App\Models;
 
+use App\Models\Buku;
+use App\Models\User;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Ulasan extends Model
 {
     use HasFactory;
+
+    public function user()
+    {
+        return $this->belongTo(User::class);
+    }
+
+    public function buku()
+    {
+        return $this->belongTo(Buku::class);
+    }
 }
+
